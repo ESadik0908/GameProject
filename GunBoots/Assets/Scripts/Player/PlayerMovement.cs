a0225b7651d74ac6154a4ec53c3185d2ef6d11cc
Ethan <1911035@swansea.ac.uk>
2023-02-28 10:03:34 +0000

    double jump added to its own script

--- GunBoots/Assets/Scripts/Player/PlayerMovement.cs
+++ GunBoots/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,21 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private bool isFacingRight = true;
 
-    private float cyoteTime = 0.1f;
-    private float cyoteTimeCounter;
-
-    private float jumpBufferTime = 0.3f;
-    private float jumpBufferCounter;
-
-    [SerializeField] private int doubleJump;
 
     [SerializeField] private float speed = 8f;
-    [SerializeField] private float jumpingPower = 16f;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
@@ -24,58 +18,12 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
-        
-        if (isGrounded())
-        {
-            doubleJump = 2;
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-
-        #region Jumping
-        if (isGrounded())
-        {
-            cyoteTimeCounter = cyoteTime;
-        }
-        else
-        {
-            cyoteTimeCounter -= Time.deltaTime;
-        }
-
-        if (Input.GetButtonDown("Jump") && doubleJump > 0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            doubleJump -= 1;
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            jumpBufferCounter = jumpBufferTime;
-        }
-        else
-        {
-            jumpBufferCounter -= Time.deltaTime;
-        }
-
-        //If you oare on the ground and the jump buffer is ready then jump
-        if (jumpBufferCounter > 0f && cyoteTimeCounter > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-
-            jumpBufferCounter = 0;
-        }
-
-        if(Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-
-            cyoteTimeCounter = 0f;
-        }
-        #endregion
 
         Flip();
     }
