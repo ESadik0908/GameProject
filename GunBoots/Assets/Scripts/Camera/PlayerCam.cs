892f92edb6db84b245f2e58e27c98ac986a89e04
Ethan <1911035@swansea.ac.uk>
2023-03-08 14:10:50 +0000

    Smooth camera

--- GunBoots/Assets/Scripts/Camera/PlayerCam.cs
+++ GunBoots/Assets/Scripts/Camera/PlayerCam.cs
@@ -5,46 +5,41 @@ using UnityEngine;
 public class PlayerCam : MonoBehaviour
 {
     public GameObject player;
-
     private PlayerMovement playerMovement;
-
     [SerializeField] private Vector3 offset = new Vector3(0, 0, -2);
-
-    [SerializeField] private float camFollowTime = 0.04f;
-
+    [SerializeField] private float camFollowTime = 0.1f;
     private Vector3 targetPosition;
-
     float yOffset = 0;
+    Vector3 target;
+    private Vector3 velocity = Vector3.zero; // added for SmoothDamp
 
-
-    // Start is called before the first frame update
     void Start()
     {
         playerMovement = player.GetComponent<PlayerMovement>();
         transform.position = player.transform.position + offset;
-
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        float directionY = Mathf.Sign(playerMovement.velocity.y);
-        offset = new Vector3(playerMovement.facing / 2, 0, -2);
+        camFollowTime = (Mathf.Abs(playerMovement.velocity.x) > 8.5) ? 0.05f : 0.1f;
+        offset = new Vector3((playerMovement.facing / 2), 0, -2);
         if (playerMovement.velocity.y < -20)
         {
             Debug.Log("speed");
             offset = new Vector3(playerMovement.facing / 2, yOffset, -2);
-            if(yOffset > -5)
+            if (yOffset > -5)
             {
-                yOffset -= Time.deltaTime * (Mathf.Abs(playerMovement.velocity.y)/10);
+                yOffset -= Time.deltaTime * (Mathf.Abs(playerMovement.velocity.y) / 10);
             }
         }
-        if(playerMovement.coyoteTimeCounter > 0)
+        if (playerMovement.coyoteTimeCounter > 0)
         {
             yOffset = 0;
         }
-        Vector3 target = player.transform.position + offset;
+        target = player.transform.position + offset;
 
-        transform.position = Vector3.Lerp(transform.position, target, camFollowTime);
+        // Use SmoothDamp instead of Lerp
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, camFollowTime);
     }
 }
+
